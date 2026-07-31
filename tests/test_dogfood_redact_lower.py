4e685f2c57bcce1d5a8d605c80b0670e62f80383
Kirill Azovtsev <kirill.azovtsev@my.games>
2026-07-31 12:39:38 +0300

    Revert "test: add regression test for case-insensitive token redaction"
    This reverts commit 3ef645ca1993a9b690376cccb06d40dfebd31a06.

--- tests/test_dogfood_redact_lower.py
+++
@@ -1,8 +0,0 @@
-from scripts.dogfood_fixture import redact_token
-
-
-def test_redact_token_is_case_insensitive_and_preserves_prefix_casing():
-    assert redact_token("token=abc123") == "token=[redacted]"
-    assert redact_token("Token=abc123") == "Token=[redacted]"
-    assert redact_token("TOKEN=abc123") == "TOKEN=[redacted]"
-    assert redact_token("ToKeN=abc123") == "ToKeN=[redacted]"
